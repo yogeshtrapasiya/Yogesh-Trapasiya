00ad66c2d91a8175d2625c53a744fd86ac7f52b8
yogeshtrapasiya <yogeshtrapasiyaa@gmail.com>
2024-06-16 18:45:44 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var sticky = $('.sticky'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= 100) sticky.addClass('fixed');
+        else sticky.removeClass('fixed');
+    });
+})
